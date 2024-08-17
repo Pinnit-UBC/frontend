e539583496aa88d67e62e31f5c5039463e3a4307
YOUR NAME HERE <richardluo73@gmail.com>
2024-08-17 03:09:48 -0700

    updated api

--- src/components/GoogleMapsScriptLoader.js
+++ src/components/GoogleMapsScriptLoader.js
@@ -6,13 +6,16 @@ const loadScript = (url) => {
   script.async = true;
   script.defer = true;
   document.head.appendChild(script);
+
+  script.onload = () => console.log("Google Maps script loaded successfully.");
+  script.onerror = (err) => console.error("Failed to load Google Maps script.", err);
 };
 
 const GoogleMapsScriptLoader = ({ apiKey, children }) => {
   useEffect(() => {
     if (!window.google) {
       try {
-        const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+        const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
         loadScript(googleMapsUrl);
       } catch (error) {
         console.error('Error loading Google Maps script:', error);
